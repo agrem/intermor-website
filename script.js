14b59e5db030e046db013037f041645cfb410e2a
agrem <57567164+agrem@users.noreply.github.com>
2026-08-12 16:13:40 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const page = location.pathname.split("/").pop() || "index.html";
-
-document.querySelectorAll(".menu a").forEach(link => {
-  if (link.getAttribute("href") === page) {
-    link.classList.add("active");
-  }
-});
